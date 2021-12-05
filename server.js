0827812f579b50b406c4c1a67449a6a1d9857b18
Ella <rafaela.morato1805@gmail.com>
2021-12-05 18:19:11 +1000

    controllers and env

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+const path = require("path");
+const routes = require("./controller");
+const bodyParser = require("body-parser");
+
+//Initialize DB
+
+require("./config/connection")();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(logger("dev"));
+app.use(compression());
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App launch on port ${PORT} 🚀`);
+});
